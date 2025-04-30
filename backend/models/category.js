69a66cb60d30b5ad1f3bf6ebd49ac802efd12549
Madan Raj M A <madanrajma.22aid@kongu.edu>
2025-04-30 09:01:42 +0530

    Update category.js

--- backend/models/category.js
+++ backend/models/category.js
@@ -1,8 +1,1 @@
-const mongoose = require('mongoose');
 
-const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  papers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Paper' }]
-});
-
-module.exports = mongoose.model('Category', categorySchema);
